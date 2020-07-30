9748b084439343965e1cb3a713182fe2b743a818
secure-revert-8990 <yuliyambg@gmail.com>
2020-07-30 20:08:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -13,7 +13,7 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-  cheese_types = ["cheddar", "gouda", "camembert"]
+  # cheese_types = ["cheddar", "gouda", "camembert"]
   
   array.find { |i| i == "cheddar" || i == "gouda" || i == "camembert" }
 end
