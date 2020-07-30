4d777fa4ec5b21a3874183457e59ca4c0418e1c5
secure-revert-8990 <yuliyambg@gmail.com>
2020-07-30 20:03:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,8 +8,8 @@ def summon_captain_planet(array)
   array.collect { |i| i.capitalize << "!" } 
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  
 end
 
 def find_the_cheese# code an argument here
