d5dc400389f80962f6d2bba1453303814ae8b517
secure-revert-8990 <yuliyambg@gmail.com>
2020-07-30 19:56:12 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,8 +4,8 @@ def roll_call_dwarves(arr)
   end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  array.
 end
 
 def long_planeteer_calls# code an argument here
